5d208cae03b8708f1eb598ac824a366729f97e09
Vladimir <basf321@yandex.ru>
2023-07-18 22:53:04 +0300

    fix: использаны переменные среды

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -8,9 +8,11 @@ import lombok.ToString;
 @Setter
 @ToString
 public class UserDto {
+
     Long id;
     String name;
     String email;
+
     public UserDto(Long id, String name, String email) {
         this.id = id;
         this.name = name;
